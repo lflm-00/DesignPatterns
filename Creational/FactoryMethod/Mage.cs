33fccb90b857a5edd8f490932639b4473aa5ded7
Luis Fernando L <luis.lopezm@pragma.com.co>
2023-10-24 10:35:05 -0500

    Add Factory Method Desing Pattern

---
+++ Creational/FactoryMethod/Mage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Factory
+{
+    internal class Mage : ICharacter
+    {
+        public void Display()
+        {
+            Console.WriteLine("I'm Mage.");
+        }
+    }
+}
---
+++ Creational/FactoryMethod/MageFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Factory
+{
+    internal class MageFactory : CharacterFactory
+    {
+        public override ICharacter CreateCharacter()
+        {
+            return new Mage();
+        }
+    }
+}

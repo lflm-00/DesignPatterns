b41aa9d5e7b781d1bc27402e3f538f3b53e88e82
Luis Fernando L <luis.lopezm@pragma.com.co>
2023-10-25 17:52:08 -0500

    Add new Desing Patter Builder

---
+++ Creational/Builder/Director.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Builder
+{
+    public class Director
+    {
+        /* Puedes personalizar la construcción del producto
+         * cambiando el orden en el que se llaman a los métodos
+         * BuildPart1, BuildPart2 y BuildPart3 en el Director.
+         */
+        private IBuilder builder;
+
+        public Director(IBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void Construct()
+        {
+            builder.BuildPart1();
+            builder.BuildPart2();
+            builder.BuildPart3();
+        }
+    }
+}
---
+++ Creational/Builder/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Builder
+{
+    public class Product
+    {
+        public string Part1 { get; set; }
+        public string Part2 { get; set; }
+        public string Part3 { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Part 1: {Part1}");
+            Console.WriteLine($"Part 2: {Part2}");
+            Console.WriteLine($"Part 3: {Part3}");
+        }
+    }
+}

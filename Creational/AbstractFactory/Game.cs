97b1a97161f076b9c867b6c639a4031d65767a84
Luis Fernando L <luis.lopezm@pragma.com.co>
2023-10-25 16:36:06 -0500

    Add new Desing Patter , Abstract Factory

---
+++ Creational/AbstractFactory/Game.cs
@@ -0,0 +1,27 @@
+﻿using Creational.AbstractFactory.AbstractProduct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.AbstractFactory
+{
+    public class Game
+    {
+        private CharacterFactory characterFactory;
+
+        public Game(CharacterFactory characterFactory)
+        {
+            this.characterFactory = characterFactory;
+        }
+        public void CreateCharacter()
+        {
+            Character character = characterFactory.CreateCharacter();
+            Weapon weapon = characterFactory.CreateWeapon();
+            Armor armor = characterFactory.CreateArmor();
+
+            Console.WriteLine($"{character.getDescription()} equipped with a {weapon.getDescription()} and wearing {armor.getDescription()}.");
+        }
+    }
+}

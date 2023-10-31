09e18fdad3ceb0f6b3e65551cdf756777e47b5d9
lflm-00 <52301606+lflm-00@users.noreply.github.com>
2023-10-31 10:18:57 -0500

    Feature/builder (#3)
    * Add Factory Method Desing Pattern
    
    * Add new Desing Patter Builder
    
    ---------
    
    Co-authored-by: Luis Fernando L <luis.lopezm@pragma.com.co>

---
+++ Creational/Builder/ConcreteBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Builder
+{
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product product = new Product();
+
+
+        public void Reset()
+        {
+            this.product = new Product();
+        }
+        public void BuildPart1()
+        {
+            product.Part1 = "Part 1 built";
+        }
+
+        public void BuildPart2()
+        {
+            product.Part2 = "Part 2 built";
+        }
+
+        public void BuildPart3()
+        {
+            product.Part3 = "Part 3 built";
+        }
+
+        public Product GetResult()
+        {
+            Product result = this.product;
+            this.Reset();
+            return result;
+        }
+    }
+}

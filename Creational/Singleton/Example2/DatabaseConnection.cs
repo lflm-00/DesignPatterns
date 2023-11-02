81d29d4cbc335496d3419b6267898945ff14129b
lflm-00 <52301606+lflm-00@users.noreply.github.com>
2023-11-02 12:32:44 -0500

    Feature/singleton (#6)
    * add new desing patter Singleton
    
    * add new desing patter Singleton
    
    ---------
    
    Co-authored-by: Luis Fernando L <luis.lopezm@pragma.com.co>

---
+++ Creational/Singleton/Example2/DatabaseConnection.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Singleton.Example2
+{
+    internal class DatabaseConnection
+    {
+        private static DatabaseConnection instance;
+        private static readonly object lockObject = new object();
+        private IDbConnection connection;
+
+        private DatabaseConnection() 
+        {
+            string connectionString = "";
+            connection = new SqlConnection(connectionString);
+        }
+
+        public static DatabaseConnection GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if(instance == null)
+                    {
+                        instance = new DatabaseConnection();
+                    }
+                }
+            }
+            return instance;   
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
